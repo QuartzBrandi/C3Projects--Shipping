8796bd6ff5cdc74d7053c85d949aa4bfaabd1057
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-19 09:52:15 -0700

    Specs.

--- spec/controllers/shipping_controller_spec.rb
+++ spec/controllers/shipping_controller_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe ShippingController, type: :controller do
+  let(:complete_query) {
+    { origin_country: "US",
+      origin_zip: "98101",
+      origin_state: "WA",
+      origin_city: "Seattle",
+      origin_address1: "301 Union St",
+      origin_address2: "PO BOX 55555-5555",
+      destination_country: "US",
+      destination_zip: "98195",
+      destination_state: "WA",
+      destination_city: "Seattle",
+      destination_address1: "University of Washington",
+      destination_address2: "",
+      ounces: "160",
+      width: "12",
+      height: "24",
+      length: "12"
+    }
+      # ounces & inches
+      # EVENTUALLY: options?
+  }
 
+  ["ups", "usps"].each do |shipper|
+    describe "GET 'show' id: #{shipper}" do
+      before :each do
+        query = complete_query
+        query[:id] = shipper
+        get :show, query
+      end
+
+      it "is successful" do
+        expect(response.response_code).to eq 200
+      end
+
+      it "returns json" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        before :each do
+          @response = JSON.parse response.body
+        end
+
+        # ex. { options: [["UPS Standard", 2345], ["UPS Global", 3456]]}
+        it "lists multiple shipment types" do
+          expect(@response["options"].count) to eq 11
+        end
+
+        it "lists shipment type and estimated cost" do
+          @response["options"].each do |estimate|
+            expect(estimate.count) to eq 2
+          end
+        end
+      end
+    end
+  end
 end
