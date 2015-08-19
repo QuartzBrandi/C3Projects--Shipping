0caa4f5aec8c4fcf1a2da930f07bfa5dcf392a8c
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-19 09:52:27 -0700

    WIP.

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -1,6 +1,21 @@
 require 'active_shipping'
 
 class ShippingController < ApplicationController
+    shipping/:id
+
+  def show
+    if params[:id] == "ups"
+      response = ups
+    elsif params[:id] == "usps"
+      response = usps
+    else
+      rsponse = []
+    end
+
+      render json: response
+  end
+
+
 
   def index
     # Package up a poster and a Wii for your nephew.
@@ -13,10 +28,23 @@ class ShippingController < ApplicationController
                                    [15, 10, 4.5],        # 15x10x4.5 inches
                                    :units => :imperial)  # not grams, not centimetres
     ]
+    #
+    # shipping/:id
+    # http://localhost:3000/shipping/q=address=US_CA_Beverly+Hills_90202&
+    # shipping?country=US&state=CA&address=BeverlyHills
+    #
+    # params[:id] = "US_CA_Beverly+Hills_90202"
+    #
+    # params[:id].split(/_/)
+    # address_info = ["US", "CA", "Beverly+Hills", "90202"]
+    # :country = address_info[0]
+    #
+    # render json: info_you_requested
+
+# small_box =
 
     # You live in Beverly Hills, he lives in Ottawa
-    origin = ActiveShipping::Location.new( :country => 'US',
-                                           :state => 'CA',
+    origin = ActiveShipping::Location.new(
                                            :city => 'Beverly Hills',
                                            :zip => '90210')
 
@@ -29,8 +57,6 @@ class ShippingController < ApplicationController
     ups = ActiveShipping::UPS.new(:login => ENV['ACTIVESHIPPING_USPS_LOGIN'], :password => ENV['ACTIVESHIPPING_UPS_PASSWORD'], :key => ENV['ACTIVESHIPPING_UPS_KEY'])
     response = ups.find_rates(origin, destination, packages)
 
-    raise
-
     ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
     # => [["UPS Standard", 3936],
     #     ["UPS Worldwide Expedited", 8682],
@@ -39,7 +65,7 @@ class ShippingController < ApplicationController
     #     ["UPS Worldwide Express Plus", 14502]]
 
     # Check out USPS for comparison...
-    usps = ActiveShipping::USPS.new(:login => 'developer-key')
+    usps = ActiveShipping::USPS.new(:login => ENV['ACTIVESHIPPING_USPS_LOGIN'])
     response = usps.find_rates(origin, destination, packages)
 
     usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
@@ -50,7 +76,7 @@ class ShippingController < ApplicationController
     #     ["USPS Global Express Guaranteed Non-Document Rectangular", 9400],
     #     ["USPS Global Express Guaranteed", 9400]]
 
-  render json: packages
+  render json: usps_rates
 
   #
   # [
@@ -78,5 +104,9 @@ class ShippingController < ApplicationController
   #   "unpackaged":false},
   #
   #   {"options":{"units":"imperial"},"dimensions":[{"amount":4.5,"unit":"inches"},{"amount":10,"unit":"inches"},{"amount":15,"unit":"inches"}],"weight_unit_system":"imperial","dimensions_unit_system":"imperial","weight":{"amount":120.0,"unit":"ounces"},"value":null,"currency":null,"cylinder":false,"gift":false,"oversized":false,"unpackaged":false}]
+  end
+
+  def show
+    render json: { ready_for_lunch: "yassss" }
   end
 end
