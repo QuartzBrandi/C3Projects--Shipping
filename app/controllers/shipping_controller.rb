9c6e3b549fd1670d98d9529a99278e757bfd4ab8
Victoria <vikshab2010@hotmail.com>
2015-08-19 13:24:05 -0700

    Added delivery date

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -5,12 +5,12 @@ class ShippingController < ApplicationController
     if params[:id]=="ups"
       ups = ActiveShipping::UPS.new(:login => ENV['ACTIVESHIPPING_UPS_LOGIN'], :password => ENV['ACTIVESHIPPING_UPS_PASSWORD'], :key => ENV['ACTIVESHIPPING_UPS_KEY'])
       response = ups.find_rates(origin, destination, packages)
-      rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, date(rate)]}
       status = 200
+      raise
     elsif params[:id] == "usps"
       usps = ActiveShipping::USPS.new(:login => ENV['ACTIVESHIPPING_USPS_LOGIN'])
       response = usps.find_rates(origin, destination, packages)
-      raise
       rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
       status = 200
     else
@@ -20,27 +20,38 @@ class ShippingController < ApplicationController
     render json: rates, status: status
   end
 
-  def origin
-    ActiveShipping::Location.new( :country => params["origin_country"],
-                                  :state => params["origin_state"],
-                                  :city => params["origin_city"],
-                                  :zip => params["origin_zip"],
-                                  :address1 => params["origin_address1"],
-                                  :address2 => params["origin_address2"])
-  end
 
-  def destination
-    ActiveShipping::Location.new( :country => params["destination_country"],
-                                  :state => params["destination_state"],
-                                  :city => params["destination_city"],
-                                  :zip => params["destination_zip"],
-                                  :address1 => params["destination_address1"],
-                                  :address2 => params["destination_address2"])
-  end
+  private
+    def origin
+      ActiveShipping::Location.new( :country => params["origin_country"],
+                                    :state => params["origin_state"],
+                                    :city => params["origin_city"],
+                                    :zip => params["origin_zip"],
+                                    :address1 => params["origin_address1"],
+                                    :address2 => params["origin_address2"])
+    end
 
-  def packages
-    ActiveShipping::Package.new( 100,              # 100 grams
-                                [93,10],           # 93 cm long, 10 cm diameter
-                                :cylinder => true)
-  end
+    def destination
+      ActiveShipping::Location.new( :country => params["destination_country"],
+                                    :state => params["destination_state"],
+                                    :city => params["destination_city"],
+                                    :zip => params["destination_zip"],
+                                    :address1 => params["destination_address1"],
+                                    :address2 => params["destination_address2"])
+    end
+
+    def packages
+      ActiveShipping::Package.new( 100,              # 100 grams
+                                  [93,10],           # 93 cm long, 10 cm diameter
+                                  :cylinder => true)
+    end
+    
+    def date(rate)
+      if rate.delivery_date != nil
+          date = Date.strptime("#{rate.delivery_date}")
+      else
+        date = rate.delivery_date
+      end
+      return date
+    end
 end
