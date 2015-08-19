2cb7935af1102b2eb4e998c7087a0126953672f7
Victoria <vikshab2010@hotmail.com>
2015-08-19 13:55:55 -0700

    Moved origin, destination and packages methods to application controller Added before_action in shipping controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def origin
+    ActiveShipping::Location.new( :country => params["origin_country"],
+                                  :state => params["origin_state"],
+                                  :city => params["origin_city"],
+                                  :zip => params["origin_zip"],
+                                  :address1 => params["origin_address1"],
+                                  :address2 => params["origin_address2"])
+  end
+
+  def destination
+    ActiveShipping::Location.new( :country => params["destination_country"],
+                                  :state => params["destination_state"],
+                                  :city => params["destination_city"],
+                                  :zip => params["destination_zip"],
+                                  :address1 => params["destination_address1"],
+                                  :address2 => params["destination_address2"])
+  end
+
+  def packages
+    ActiveShipping::Package.new( 100,              # 100 grams
+                                [93,10],           # 93 cm long, 10 cm diameter
+                                :cylinder => true)
+  end
+
 end
--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -1,50 +1,28 @@
 require 'active_shipping'
-
 class ShippingController < ApplicationController
-  def show
-    if params[:id]=="ups"
-      rates = find_rates(ups_carrier)
-      rates = sort_rates(rates)
-      status = 200
-    elsif params[:id] == "usps"
-      rates = find_rates(usps_carrier)
-      rates = sort_rates(rates)
-      status = 200
-    else
-      rates = []
-      status = 204
+  before_action :origin, :destination, :packages
+
+    def show
+      if params[:id]=="ups"
+        rates = find_rates(ups_carrier)
+        rates = sort_rates(rates)
+        status = 200
+      elsif params[:id] == "usps"
+        rates = find_rates(usps_carrier)
+        rates = sort_rates(rates)
+        status = 200
+      else
+        rates = []
+        status = 204
+      end
+      render json: rates, status: status
     end
-    render json: rates, status: status
-  end
 
   private
-    def origin
-      ActiveShipping::Location.new( :country => params["origin_country"],
-                                    :state => params["origin_state"],
-                                    :city => params["origin_city"],
-                                    :zip => params["origin_zip"],
-                                    :address1 => params["origin_address1"],
-                                    :address2 => params["origin_address2"])
-    end
-
-    def destination
-      ActiveShipping::Location.new( :country => params["destination_country"],
-                                    :state => params["destination_state"],
-                                    :city => params["destination_city"],
-                                    :zip => params["destination_zip"],
-                                    :address1 => params["destination_address1"],
-                                    :address2 => params["destination_address2"])
-    end
-
-    def packages
-      ActiveShipping::Package.new( 100,              # 100 grams
-                                  [93,10],           # 93 cm long, 10 cm diameter
-                                  :cylinder => true)
-    end
 
     def delivery_date(rate)
       if rate.delivery_date != nil
-          date = Date.strptime("#{rate.delivery_date}")
+        date = Date.strptime("#{rate.delivery_date}")
       else
         date = rate.delivery_date
       end
