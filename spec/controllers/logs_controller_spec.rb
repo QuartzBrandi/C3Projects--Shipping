a13de9ea73de9dcf2c35d05441960f3239a85690
Victoria <vikshab2010@hotmail.com>
2015-08-20 14:05:19 -0700

    Tests for log controller, 100% coverage

--- spec/controllers/logs_controller_spec.rb
+++ spec/controllers/logs_controller_spec.rb
@@ -2,4 +2,35 @@ require 'rails_helper'
 
 RSpec.describe LogsController, type: :controller do
 
+  describe "Create method" do
+    let(:params) {
+      {
+      :id => "tux",
+      :order_number => "21",
+      :provider => "UPS Ground",
+      :cost => "1125",
+      :estimate => "",
+      :purchase_time => "2015-08-20 20:45:59 UTC"
+      }
+    }
+    before :each do
+      post :create, params
+    end
+
+    it "is successful" do
+      expect(response.response_code).to eq 200
+    end
+
+    it "returns json" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    it "creates a record in Logs table" do
+      expect(Log.count).to eq 1
+    end
+
+    it "has 'UPS Ground' provider" do
+      expect(Log.first.provider).to eq "UPS Ground"
+    end
+  end
 end
