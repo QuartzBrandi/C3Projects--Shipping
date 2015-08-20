2edc9963b860f5c5e189c387205a29a5744a48bc
Victoria <vikshab2010@hotmail.com>
2015-08-19 19:56:20 -0700

    Added new tests

--- spec/controllers/shipping_controller_spec.rb
+++ spec/controllers/shipping_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ShippingController, type: :controller do
-  let(:complete_query) {
+  let(:valid_query) {
     { origin_country: "US",
       origin_zip: "98101",
       origin_state: "WA",
@@ -14,19 +14,29 @@ RSpec.describe ShippingController, type: :controller do
       destination_city: "Seattle",
       destination_address1: "University of Washington",
       destination_address2: "",
-      # ounces: "160",
-      # width: "12",
-      # height: "24",
-      # length: "12"
     }
-      # ounces & inches
-      # EVENTUALLY: options?
+  }
+
+  let(:invalid_query) {
+    { origin_country: "US",
+      origin_zip: "98101",
+      origin_state: "WA",
+      origin_city: "Seattle",
+      origin_address1: "301 Union St",
+      origin_address2: "PO BOX 55555-5555",
+      destination_country: "US",
+      destination_zip: "98",
+      destination_state: "WA",
+      destination_city: "Seattle",
+      destination_address1: "University of Washington",
+      destination_address2: "",
+    }
   }
 
   {"ups" => 7, "usps" => 6}.each do |shipper, count|
     describe "GET 'show' id: #{shipper}" do
       before :each do
-        query = complete_query
+        query = valid_query
         query[:id] = shipper
         get :show, query
       end
@@ -55,6 +65,40 @@ RSpec.describe ShippingController, type: :controller do
           end
         end
       end
+    end
+  end
+
+  describe "fedex is invalid carrier" do
+    before :each do
+      query = valid_query
+      query[:id] = "fedex"
+      get :show, query
+    end
+
+    it "is has 400 code status" do
+      expect(response.response_code).to eq 400
+    end
+
+    it "displays error message 'Ivalid carrier' for fedex" do
+      @response = JSON.parse response.body
+      expect(@response["message"]).to eq "Invalid carrier"
+    end
+  end
+
+  describe "zip code is invalid address" do
+    before :each do
+      query = invalid_query
+      query[:id] = "ups"
+      get :show, query
+    end
+
+    it "is has 400 code status" do
+      expect(response.response_code).to eq 400
+    end
+
+    it "displays error message 'Ivalid address' for fedex" do
+      @response = JSON.parse response.body
+      expect(@response["message"]).to eq "Invalid address"
     end
   end
 end
